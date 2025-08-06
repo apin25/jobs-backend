2ea1a9632d9db8eb54971af13d83e5e732de01d5
Alvin Widi Nugroho <alvin.widi@ui.ac.id>
2025-08-06 14:18:25 +0700

    fixing url /me

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path('/me/<str:token>', get_user_by_token, name="me")
+    path('me/<str:token>', get_user_by_token, name="me")
 ]
