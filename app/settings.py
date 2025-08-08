6bb07366cda454ec7b87f4bbfba86ea9f69b7539
Alvin Widi Nugroho <alvin.widi@ui.ac.id>
2025-08-08 14:02:31 +0700

    fixing cors

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-)=jm(=pqmbc+xxk0*ee%@_)4zizamf$j))zmqdui=m2wlfllt!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1','vercel.app']
+CORS_ALLOWED_ORIGINS = [
+    "http://10.0.2.2:19006",  
+    "http://localhost:19006", 
+]
 
 
 # Application definition
